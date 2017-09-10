12aae2f2541981b0524ea7e9a333a1f625f480f2
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2017-09-10 14:49:58 +0600

    Add react components & style

---
+++ src/Calculator.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import PointTarget from 'react-point';
+
+class AutoScalingText extends React.Component {
+  state = {
+    scale: 1
+  };
+
+  componentDidUpdate() {
+    const { scale } = this.state
+
+    const node = this.node
+    const parentNode = node.parentNode
+
+    const availableWidth = parentNode.offsetWidth
+    const actualWidth = node.offsetWidth
+    const actualScale = availableWidth / actualWidth
+
+    if (scale === actualScale)
+      return
+
+    if (actualScale < 1) {
+      this.setState({ scale: actualScale })
+    } else if (scale < 1) {
+      this.setState({ scale: 1 })
+    }
+  }
+
+  render() {
+    const { scale } = this.state
+
+    return (
+      <div
+        className="auto-scaling-text"
+        style={{ transform: `scale(${scale},${scale})` }}
+        ref={node => this.node = node}
+      >{this.props.children}</div>
+    )
+  }
+}
+
+export class CalculatorDisplay extends React.Component {
+  render() {
+    const { value, ...props } = this.props
+
+    const language = navigator.language || 'en-US'
+    let formattedValue = parseFloat(value).toLocaleString(language, {
+      useGrouping: true,
+      maximumFractionDigits: 6
+    })
+
+    // Add back missing .0 in e.g. 12.0
+    const match = value.match(/\.\d*?(0*)$/)
+
+    if (match)
+      formattedValue += (/[1-9]/).test(match[0]) ? match[1] : match[0]
+
+    return (
+      <div {...props} className="calculator-display">
+        <AutoScalingText>{formattedValue}</AutoScalingText>
+      </div>
+    )
+  }
+}
+
+export class CalculatorKey extends React.Component {
+  render() {
+    const { onPress, className, ...props } = this.props
+
+    return (
+      <PointTarget onPoint={onPress}>
+        <button className={`calculator-key ${className}`} {...props}/>
+      </PointTarget>
+    )
+  }
+}
